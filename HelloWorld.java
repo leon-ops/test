dd4efe95b9f996113b6cc92cd91c32fda099b970
leon-ops <leon-ops@users.noreply.github.com>
2022-02-08 18:01:57 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+        System.out.println("Hello World!"); 
+        System.out.println("i18n test 人类 ");
     }
 }
